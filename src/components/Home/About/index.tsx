5b360d60f30045974865f1949272b4e8550d6c0b
Vinicius Leão <viniciussantanaleao@gmail.com>
2023-03-21 23:50:08 -0300

    feat: add baby yoda

--- src/components/Home/About/index.tsx
+++ src/components/Home/About/index.tsx
@@ -1,11 +1,13 @@
 import { AboutDescription } from "./Description";
 import { AboutProfile } from "./Profile";
+import BabyYoda from "src/assets/baby-yoda.png";
 import styles from "./styles.module.scss";
 
 export function About() {
   return (
     <section className={styles["about-container"]}>
       <div className={styles["about-content"]}>
+        <img src={BabyYoda} className={styles["about-baby-yoda"]} />
         <AboutProfile />
         <AboutDescription />
       </div>
